3513f22437c556ea23a72c3162e187f198873cea
jackson_wuerfl <jackson_wuerfl@student.wayland.k12.ma.us>
2019-06-03 08:05:56 -0400

    Added Main.java

--- src/BreakerMain.java
+++ src/BreakerMain.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BreakerMain extends JPanel {
 
@@ -9,6 +11,13 @@ public class BreakerMain extends JPanel {
 
 
     public BreakerMain(){
+
+        timer = new Timer(40, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
 
 
 
