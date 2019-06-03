7551d581d7e2a65a2d402592afd7542630193095
jackson_wuerfl <jackson_wuerfl@student.wayland.k12.ma.us>
2019-06-03 07:59:47 -0400

    Added Main.java

--- src/BreakerMain.java
+++ src/BreakerMain.java
@@ -14,6 +14,21 @@ public class BreakerMain extends JPanel {
 
     }
 
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Brick Breaker!");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
 
+        BreakerMain panel = new BreakerMain();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
     }
+}
+
 
