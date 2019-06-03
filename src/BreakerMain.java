3f2e16a000d4816ff9bceed5a8ed82bcca8c4d70
jackson_wuerfl <jackson_wuerfl@student.wayland.k12.ma.us>
2019-06-03 07:55:30 -0400

    Added Main.java

---
+++ src/BreakerMain.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class BreakerMain extends JPanel {
+
+    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 700;
+    private Timer timer;
+
+
+
+
+    public BreakerMain(){
+
+
+
+    }
+
+
+    }
+
